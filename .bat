c72daa0f7f1756b835fc736e26332987bd447314
bullet34 <bullet34@naver.com>
2025-06-05 16:39:33 +0900

    Update .bat

--- .bat
+++ .bat
@@ -4,9 +4,9 @@
 
 title start_test
 
-cd C:\Users\Samsung\projectD
+cd C:\Users\project  // 실행파일 경로
 
-call pj_d/scripts/activate
-python test2.py
+call project_test/scripts/activate  // 가상환경의 인터프리터 지정
+python test1.py  // test1.py 실행
 
 cmd.exe
